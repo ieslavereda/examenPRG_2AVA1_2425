7d53230915673042779a3cdf2341dae6659c07ad
xrosilloprofe <xv.rosillo@edu.gva.es>
2024-11-19 17:06:01 +0100

    v0.9

---
+++ src/Ejercicio04.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Ejercicio04 {
+    public static void main(String[] args) {
+        int[][] matriz = dibujaCruz(5);
+        for (int[] array:matriz)
+            System.out.println(Arrays.toString(array));
+    }
+
+    public static int[][] dibujaCruz(int numero){
+        int[][] matriz = new int[numero][numero];
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                if(i==j || j==matriz.length-i-1)
+                    matriz[i][j] = 1;
+                else
+                    matriz[i][j] = 0;
+            }
+        }
+        return matriz;
+    }
+
+}
---
+++ src/Ejercicio05.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Ejercicio05 {
+    public static void main(String[] args) {
+        int[] array = new int[9];
+        rellenarArray(array);
+        System.out.println(Arrays.toString(array));
+        invertirArray(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void rellenarArray(int[] array){
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int)(Math.random()*10)+1;
+        }
+    }
+
+    public static void invertirArray(int[] array){
+        for (int i = 0; i < array.length/2; i++) {
+            int aux = array[i];
+            array[i] = array[array.length-i-1];
+            array[array.length-i-1] = aux;
+        }
+    }
+
+}
